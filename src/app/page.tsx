15539b8cab7a7c648dc494f8709cca7f0df197e4
Saurabh Anand <saurabhbebi@gmail.com>
2024-01-29 13:23:40 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import HomePage from "@/components/HomePage";
 import Navbar from "@/components/Navbar";
+import  Profile  from "@/components/Profile";
 export default function Home() {
   return (
-    <div className="font-serif">
+    <div className="font-serif overflow-hidden">
     <Navbar/>
     <HomePage/>
+    <Profile/>
     </div>
   );
 }
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,21 +6,21 @@ const HomePage = () => {
     console.log("hi");
   };
   return (
-    <div className=" flex justify-between  px-40 pt-5 pb-2 gap-3  w-full">
-      <div className="flex-col justify-between  md:px-10 md:pt-2 md:pb-2 w-full md:w-5/12 ">
-        <img
+    <div className="sm:rounded-tl-[500px] rounded-tl-[180px]  bg-[#FFF7ED]  sm:flex justify-between  sm:px-40 px-4 pt-5 pb-2 gap-3  w-full   ">
+      <div className="flex-col  justify-between  md:px-10 md:pt-2 md:pb-2 w-full md:w-5/12 ">
+        <img className="rounded-b-full rounded-tl-full "
           alt="WEB Dev."
           src="https://img.freepik.com/premium-vector/programmer-focused-work-project-while-sitting-easy-chair_569013-330.jpg?w=740"
         ></img>
         <p>Warm Welcome!</p>
-        <p className="text-[3vw]">My Name is Saurabh Anand Dev.</p>
+        <p className="text-[2rem]">Full Stack Devloper.</p>
       </div>
-      <div className="flex items-center justify-center md:px-10 md:pt-2 md:pb-2  w-full md:w-5/12">
+      <div className="  sm:flex items-center justify-center sm:px-10 pt-2 pb-2  w-full md:w-5/12">
         <div className="flex-col justify-center ">
           <p className="text-xl">
             Btech 2nd Year Student,Working on My Skills Still Learning,Open For Oppurtunities.
           </p>
-          <Button handle={handleParty} text="Click Here For Perks" />
+          <Button handle={handleParty} text="Click Here For Firework" />
         </div>
       </div>
     </div>
