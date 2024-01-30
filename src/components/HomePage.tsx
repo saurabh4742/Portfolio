3cd1f0ebd94a530625d75e2bb6de4b880ddd887c
Saurabh Anand <saurabhbebi@gmail.com>
2024-01-30 08:51:45 +0530

    update

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,12 +6,12 @@ const HomePage = () => {
     console.log("hi");
   };
   return (
-    <div className="sm:rounded-tl-[500px] rounded-tl-[180px]  bg-[#FFF7ED]  sm:flex justify-between  sm:px-40 px-4 pt-5 pb-2 gap-3  w-full   ">
-      <div className="flex-col  justify-between  md:px-10  w-full md:w-5/12 ">
+    <div className="sm:rounded-tl-[500px] rounded-tl-[180px]  bg-[#FFF7ED]  sm:flex justify-between  sm:px-20 px-4 pt-5 pb-2 gap-3  w-full   ">
+      <div className="flex-col  justify-between  sm:px-10  w-full sm:w-5/12 ">
         <WebDeveloper/>
       </div>
       <div className=" sm:flex items-center justify-between sm:py-36  py-2 flex-col  w-full md:w-5/12">
-      <p className="text-[2rem]">Full Stack Developer</p>
+      <p className="text-3xl sm:text-nowrap py-2">Full Stack Developer</p>
           <p className="sm:text-xl text-lg sm:text-center">
             Btech 2nd Year Student at NIET,Working on My Skills Still Learning,Open For Oppurtunities.
           </p>
