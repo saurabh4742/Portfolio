82ddf0a95da3bb2cba59cea4ed2cabc7d62d5b1c
Saurabh Anand <saurabhbebi@gmail.com>
2024-02-11 14:20:55 +0530

    update

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async (): Promise<void> => {
   try {
-    const connection = await mongoose.connect("mongodb+srv://saurabhbebi:bhimkishakti@cluster0.nyfiw0q.mongodb.net/");
-
+    const connection = await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log(`Connected to MongoDB Atlas: ${connection.connection.host}`);
   } catch (error: any) {
     console.error(`Error connecting to MongoDB Atlas: ${error.message}`);
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,9 +1,8 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
-require("dotenv").config();
 const firebaseConfig = {
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "portfolio-2db20.appspot.com",
+  storageBucket: process.env.STORAGE_BUCKET,
 };
 
 admin.initializeApp(firebaseConfig);
