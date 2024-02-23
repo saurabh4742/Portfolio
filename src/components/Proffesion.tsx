8c50644e65435cb53d3a88ad264af9458d438960
saurabh anand <saurabhbebi@gmail.com>
2024-02-23 19:10:48 +0530

    update

--- src/components/Proffesion.tsx
+++ src/components/Proffesion.tsx
@@ -1,22 +1,31 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 
 const TypingAnimation = () => {
   const [text, setText] = useState("");
-  const fullText = "Full Stack Developer";
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  const fullTexts = useMemo(() => ["MERN Stack Developer", "NextJS Developer"], []);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
+      const fullText = fullTexts[currentTextIndex];
       const nextChar = fullText[text.length];
       if (nextChar !== undefined) {
         setText((prevText) => prevText + nextChar);
       } else {
         clearInterval(intervalId);
+        setTimeout(() => {
+          setText("");
+          setCurrentTextIndex((prevIndex) =>
+            (prevIndex + 1) % fullTexts.length
+          );
+        }, 1000);
       }
     }, 100);
 
     return () => clearInterval(intervalId);
-  }, [text]);
+  }, [text, currentTextIndex, fullTexts]);
 
   return (
     <div className="flex items-center">
